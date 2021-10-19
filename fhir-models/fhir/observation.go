1c0512770fe0f39672c43d75a91e78c183306783
bilalgulzar <bilal.gulzar@recaresolutions.com>
2021-10-19 17:32:27 +0200

    adding duplicate models for encrypted fields

--- fhir-models/fhir/observation.go
+++ fhir-models/fhir/observation.go
@@ -56,6 +56,44 @@ type Observation struct {
 	EffectiveTiming   *Timing                     `bson:"effectiveTiming,omitempty" json:"effectiveTiming,omitempty"`
 	EffectiveInstant  *bool                       `bson:"effectiveInstant,omitempty" json:"effectiveInstant,omitempty"`
 }
+
+type RecareObservation struct {
+	Id                *string                     `bson:"id,omitempty" json:"id,omitempty"`
+	Meta              *Meta                       `bson:"meta,omitempty" json:"meta,omitempty"`
+	ImplicitRules     *string                     `bson:"implicitRules,omitempty" json:"implicitRules,omitempty"`
+	Language          *string                     `bson:"language,omitempty" json:"language,omitempty"`
+	Text              *Narrative                  `bson:"text,omitempty" json:"text,omitempty"`
+	Extension         []Extension                 `bson:"extension,omitempty" json:"extension,omitempty"`
+	ModifierExtension []Extension                 `bson:"modifierExtension,omitempty" json:"modifierExtension,omitempty"`
+	Identifier        []Identifier                `bson:"identifier,omitempty" json:"identifier,omitempty"`
+	BasedOn           []Reference                 `bson:"basedOn,omitempty" json:"basedOn,omitempty"`
+	PartOf            []Reference                 `bson:"partOf,omitempty" json:"partOf,omitempty"`
+	Status            ObservationStatus           `bson:"status" json:"status"`
+	Category          []CodeableConcept           `bson:"category,omitempty" json:"category,omitempty"`
+	Code              CodeableConcept             `bson:"code" json:"code"`
+	Subject           *Reference                  `bson:"subject,omitempty" json:"subject,omitempty"`
+	Focus             []Reference                 `bson:"focus,omitempty" json:"focus,omitempty"`
+	Encounter         *Reference                  `bson:"encounter,omitempty" json:"encounter,omitempty"`
+	Issued            *string                     `bson:"issued,omitempty" json:"issued,omitempty"`
+	Performer         []Reference                 `bson:"performer,omitempty" json:"performer,omitempty"`
+	DataAbsentReason  *CodeableConcept            `bson:"dataAbsentReason,omitempty" json:"dataAbsentReason,omitempty"`
+	Interpretation    []CodeableConcept           `bson:"interpretation,omitempty" json:"interpretation,omitempty"`
+	Note              []Annotation                `bson:"note,omitempty" json:"note,omitempty"`
+	BodySite          *CodeableConcept            `bson:"bodySite,omitempty" json:"bodySite,omitempty"`
+	Method            *CodeableConcept            `bson:"method,omitempty" json:"method,omitempty"`
+	Specimen          *Reference                  `bson:"specimen,omitempty" json:"specimen,omitempty"`
+	Device            *Reference                  `bson:"device,omitempty" json:"device,omitempty"`
+	ReferenceRange    []ObservationReferenceRange `bson:"referenceRange,omitempty" json:"referenceRange,omitempty"`
+	HasMember         []Reference                 `bson:"hasMember,omitempty" json:"hasMember,omitempty"`
+	DerivedFrom       []Reference                 `bson:"derivedFrom,omitempty" json:"derivedFrom,omitempty"`
+	Component         []ObservationComponent      `bson:"component,omitempty" json:"component,omitempty"`
+	ValueQuantity     *RecareQuantity             `bson:"valueQuantity,omitempty" json:"valueQuantity,omitempty"`
+	EffectiveDateTime *string                     `bson:"effectiveDateTime,omitempty" json:"effectiveDateTime,omitempty"`
+	EffectivePeriod   *Period                     `bson:"effectivePeriod,omitempty" json:"effectivePeriod,omitempty"`
+	EffectiveTiming   *Timing                     `bson:"effectiveTiming,omitempty" json:"effectiveTiming,omitempty"`
+	EffectiveInstant  *bool                       `bson:"effectiveInstant,omitempty" json:"effectiveInstant,omitempty"`
+}
+
 type ObservationReferenceRange struct {
 	Id                *string           `bson:"id,omitempty" json:"id,omitempty"`
 	Extension         []Extension       `bson:"extension,omitempty" json:"extension,omitempty"`
@@ -89,9 +127,29 @@ func (r Observation) MarshalJSON() ([]byte, error) {
 	})
 }
 
+type RecareOtherObservation RecareObservation
+
+func (r RecareObservation) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		RecareOtherObservation
+		ResourceType string `json:"resourceType"`
+	}{
+		RecareOtherObservation: RecareOtherObservation(r),
+		ResourceType:           "RecareObservation",
+	})
+}
+
 // UnmarshalObservation unmarshals a Observation.
 func UnmarshalObservation(b []byte) (Observation, error) {
 	var observation Observation
+	if err := json.Unmarshal(b, &observation); err != nil {
+		return observation, err
+	}
+	return observation, nil
+}
+
+func UnmarshalRecareObservation(b []byte) (RecareObservation, error) {
+	var observation RecareObservation
 	if err := json.Unmarshal(b, &observation); err != nil {
 		return observation, err
 	}
